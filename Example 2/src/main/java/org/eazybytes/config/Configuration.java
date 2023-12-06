c72afbad67f39d54b3e85fda0e04af73829621f2
rishiwalanjkar <rishi.walanjkar@gmaile.com>
2023-12-06 19:11:48 +0530

    Example 2

---
+++ Example 2/src/main/java/org/eazybytes/config/Configuration.java
@@ -0,0 +1,33 @@
+package org.eazybytes.config;
+
+import org.eazybytes.bean.Vehicle;
+import org.springframework.context.annotation.Bean;
+
+@org.springframework.context.annotation.Configuration
+public class Configuration {
+
+    @Bean
+    public Vehicle vehicle1() {
+        Vehicle vehicle = new Vehicle();
+        vehicle.setName("Ford EcoSport");
+
+        return vehicle;
+    }
+
+    @Bean
+    public Vehicle vehicle2() {
+        Vehicle vehicle = new Vehicle();
+        vehicle.setName("Honda Amaze");
+
+        return vehicle;
+    }
+
+    @Bean
+    public Vehicle vehicle3() {
+        Vehicle vehicle = new Vehicle();
+        vehicle.setName("Tata Punch");
+
+        return vehicle;
+    }
+
+}
