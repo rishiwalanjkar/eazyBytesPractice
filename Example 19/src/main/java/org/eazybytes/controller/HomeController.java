b8fc3ed68f72d851864aa5222c463c5928613a79
rishiwalanjkar <rishi.walanjkar@gmaile.com>
2024-02-04 14:51:56 +0530

    Hello World Web App using Thymeleaf & DevTools Demo

---
+++ Example 19/src/main/java/org/eazybytes/controller/HomeController.java
@@ -0,0 +1,16 @@
+package org.eazybytes.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping(value = {"", "/", "home"})
+    public String displayHomePage(Model model) {
+        model.addAttribute("username", "Rishi");
+
+        return "home";
+    }
+}
