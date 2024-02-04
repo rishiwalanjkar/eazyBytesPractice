4b81d0c15173194c6fa88110726e090f62c5ac43
rishiwalanjkar <rishi.walanjkar@gmaile.com>
2024-02-04 13:07:38 +0530

    Hello World Web App using SpringBoot & Spring MVC

---
+++ Example 18/src/main/java/org/eazybytes/EazySchoolApplication.java
@@ -0,0 +1,16 @@
+package org.eazybytes;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EazySchoolApplication {
+    public static void main(String[] args) {
+        // Press Alt+Enter with your caret at the highlighted text to see how
+        // IntelliJ IDEA suggests fixing it.
+        SpringApplication.run(EazySchoolApplication.class, args);
+    }
+}
