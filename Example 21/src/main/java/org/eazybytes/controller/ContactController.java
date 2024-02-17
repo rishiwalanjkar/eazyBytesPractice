3fc215fbc8abe7a793190ec8f89a8675c29261d3
rishiwalanjkar <rishi.walanjkar@gmaile.com>
2024-02-17 20:46:12 +0530

    Build EazySchool Courses, About, Contact Pages & Intro to Lombok

--- Example 21/src/main/java/org/eazybytes/controller/ContactController.java
+++ Example 21/src/main/java/org/eazybytes/controller/ContactController.java
@@ -1,6 +1,7 @@
 package org.eazybytes.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.eazybytes.model.Contact;
 import org.eazybytes.service.ContactService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -21,14 +22,20 @@ public class ContactController {
         return "contact.html";
     }
 
-    @PostMapping("/saveMsg")
-    public ModelAndView saveMessage(@RequestParam String name, @RequestParam String mobileNum, @RequestParam String email, @RequestParam String subject, @RequestParam String message) {
-        log.info("Name : " + name);
-        log.info("Mobile Number : " + mobileNum);
-        log.info("Email Id : " + email);
-        log.info("Subject : " + subject);
-        log.info("Message : " + message);
+//    @PostMapping("/saveMsg")
+//    public ModelAndView saveMessage(@RequestParam String name, @RequestParam String mobileNum, @RequestParam String email, @RequestParam String subject, @RequestParam String message) {
+//        log.info("Name : " + name);
+//        log.info("Mobile Number : " + mobileNum);
+//        log.info("Email Id : " + email);
+//        log.info("Subject : " + subject);
+//        log.info("Message : " + message);
+//
+//        return new ModelAndView("redirect:/contact");
+//    }
 
+    @PostMapping("/saveMsg")
+    public ModelAndView saveMessage(Contact contact) {
+        contactService.saveContactDetails(contact);
         return new ModelAndView("redirect:/contact");
     }
 }
